85921e1b7b877eb68423817b36564c92c1b7283c
Gyanthakur <gps.96169@gmail.com>
2024-05-01 23:36:14 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { EdgeStoreProvider } from "@/lib/edgestore";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nots-store",
-  description: "store appears to be a misspelling of Notion,Notion is a popular productivity and note-taking app designed to help users organize their thoughts, manage projects, and collaborate effectively. It offers a variety of features, including:",
+  title: "Notes-store",
+  description: "Notes_store appears to be a misspelling of Notion,Notion is a popular productivity and note-taking app designed to help users organize their thoughts, manage projects, and collaborate effectively. It offers a variety of features, including:",
   icons:{
     icon:[
       {
