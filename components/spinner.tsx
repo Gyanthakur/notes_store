7fd0c2b4618ac1d92fce779db57fb61e43a65cb1
Gyanthakur <gps.96169@gmail.com>
2024-12-15 00:11:16 +0530

    update: spinner

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,32 +1,57 @@
 import { Loader } from "lucide-react";
+
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-
-const spinnerVariants = cva(
-    "text-muted-foreground animated-spin",
-    {
-        variants:{
-            size:{
-                default:"h-4 w-4",
-                sm:"h-2 w-2",
-                lg:"h-6 w-6",
-                icon: "h-10 w-10"
-            }
-        },
-        defaultVariants:{
-            size: "default",
-        },
+const spinnervariants = cva("text-muted-foreground animate-spin", {
+  variants: {
+    size: {
+      default: "h-4 w-4",
+      sm: "h-2 w-2",
+      lg: "h-6 w-6",
+      icon: "h-10 w-10",
     },
-);
-interface SpinnerProps extends VariantProps<typeof spinnerVariants>{}
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+interface SpinnerProps extends VariantProps<typeof spinnervariants> {}
+
+export const Spinner = ({ size }: SpinnerProps) => {
+  return <Loader className={cn(spinnervariants({ size }))} />;
+};
 
 
-export const Spinner = ({
-    size,
-}:SpinnerProps)=>{
-    return (
-        <Loader className={cn(spinnerVariants({size}))}/>
-    );
+
+// import { Loader } from "lucide-react";
+// import { cva, type VariantProps } from "class-variance-authority";
+// import { cn } from "@/lib/utils";
+
+// const spinnerVariants = cva(
+//     "text-muted-foreground animated-spin",
+//     {
+//         variants:{
+//             size:{
+//                 default:"h-4 w-4",
+//                 sm:"h-2 w-2",
+//                 lg:"h-6 w-6",
+//                 icon: "h-10 w-10"
+//             }
+//         },
+//         defaultVariants:{
+//             size: "default",
+//         },
+//     },
+// );
+// interface SpinnerProps extends VariantProps<typeof spinnerVariants>{}
 
 
-} 
+// export const Spinner = ({
+//     size,
+// }:SpinnerProps)=>{
+//     return (
+//         <Loader className={cn(spinnerVariants({size}))}/>
+//     );
+
+
+// } 
